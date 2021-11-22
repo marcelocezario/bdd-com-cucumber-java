19d1ca6a61fe50dedda34e6aa38f0435b293edb3
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-22 14:42:06 -0300

    Aplicando Cucumber

---
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,20 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AprenderCucumber {
+
+	@Given("^Dado que criei o arquivo corretamente$")
+	public void dado_que_criei_o_arquivo_corretamente() throws Throwable {
+	}
+
+	@When("^Quanto executá-lo$")
+	public void quanto_executá_lo() throws Throwable {
+	}
+
+	@Then("^Então a especificação deve finalizar com sucesso$")
+	public void então_a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	}
+
+}
