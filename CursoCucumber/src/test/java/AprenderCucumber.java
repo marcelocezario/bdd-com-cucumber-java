30b4eb7e715741d8fc45262a838df344409f59d6
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-22 18:03:23 -0300

    Utilizando regex

--- CursoCucumber/src/test/java/AprenderCucumber.java
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -69,8 +69,31 @@ public class AprenderCucumber {
 	public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
 		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
 		String dataFormatada = format.format(entrega);
-		Assert.assertEquals(data, dataFormatada);
-		
+		Assert.assertEquals(data, dataFormatada);	
+	}
+	
+	@Dado("^que o ticket( especial)? é (A.\\d{3})$")
+	public void queOTicketÉAF(String tipo, String arg1) throws Throwable {
+	}
+
+	@Dado("^que o valor da passagem é R\\$ (\\d+),(\\d+)$")
+	public void queOValorDaPassagemÉR$(int arg1, int arg2) throws Throwable {
+	}
+
+	@Dado("^que o nome do passageiro é \"(.{5,20})\"$")
+	public void queONomeDoPassageiroÉ(String arg1) throws Throwable {
+	}
+
+	@Dado("^que o telefone do passageiro é (9\\d{3}-\\d{4})$")
+	public void queOTelefoneDoPassageiroÉ(String telefone) throws Throwable {
+	}
+
+	@Quando("^criar os steps$")
+	public void criarOsSteps() throws Throwable {
+	}
+
+	@Então("^o teste vai funcionar$")
+	public void oTesteVaiFuncionar() throws Throwable {
 	}
 
 }
