cef46c1ae6ebb138335c3091b68d3882f712552f
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-23 10:12:36 -0300

    Criando primeiro cenário de locadora

---
+++ CursoCucumber/src/main/java/br/ce/wcaquino/entidades/Filme.java
@@ -0,0 +1,23 @@
+package br.ce.wcaquino.entidades;
+
+public class Filme {
+
+	private int estoque;
+	private int aluguel;
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+
+	public int getAluguel() {
+		return aluguel;
+	}
+
+	public void setAluguel(int aluguel) {
+		this.aluguel = aluguel;
+	}
+}
--- CursoCucumber/src/test/java/br/ce/wcaquino/runners/Runner.java
+++ CursoCucumber/src/test/java/br/ce/wcaquino/runners/Runner.java
@@ -7,9 +7,9 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/aprender_cucumber.feature",
+		features = "src/test/resources/features/alugar_filme.feature",
 		glue = "br.ce.wcaquino.steps",
-		tags = {"~@ignore, @tipo1, @tipo2"},
+		tags = {},
 		plugin = "pretty",
 		monochrome = true,
 		snippets = SnippetType.CAMELCASE,
