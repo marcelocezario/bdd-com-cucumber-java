ea0c060a0944111d7b2cb41ed17db39c99dc5391
Marcelo Henrique Cezário <marcelocezario@gmail.com>
2021-11-23 14:36:40 -0300

    Tirando screenshots dos testes

--- CursoCucumber/src/test/java/br/ce/wcaquino/steps/InserirContasSteps.java
+++ CursoCucumber/src/test/java/br/ce/wcaquino/steps/InserirContasSteps.java
@@ -1,11 +1,18 @@
 package br.ce.wcaquino.steps;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import cucumber.api.PendingException;
+import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
@@ -95,7 +102,17 @@ public class InserirContasSteps {
 		System.out.println("Começando aqui");
 	}
 	
-	@After
+	@After(order = 1)
+	public void screenshot(Scenario cenario) {
+		File file = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		try {
+			FileUtils.copyFile(file, new File("target/screenshot/" + cenario.getId() +".jpg"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@After(order = 0)
 	public void fecharBrowser() {
 		driver.quit();
 		System.out.println("Terminando");
